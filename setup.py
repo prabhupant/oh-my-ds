a92eb9f88cb947d15a2a76e771a47e072951e896
Prabhu <prabhupant09@gmail.com>
2021-05-23 12:49:36 +0530

    Upgraded version from 0.0.1 to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ohmyds",
-    version="0.0.1",
+    version="0.0.2",
     author="Prabhu Pant",
     author_email="prabhupant09@gmail.com",
     description="Python library to visualize data strcutures",
